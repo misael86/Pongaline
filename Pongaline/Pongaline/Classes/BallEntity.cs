6c3d4fc0886f26ac7d0373c240127aa3512a5b57
Misael Berrios Salas <j.elmasou@gmail.com>
2012-11-10 19:48:22 +0100

    Added counter, a bit buggy

--- Pongaline/Pongaline/Classes/BallEntity.cs
+++ Pongaline/Pongaline/Classes/BallEntity.cs
@@ -26,7 +26,7 @@ namespace Pongaline.Classes
             if (translateTransform.X > GlobalMethods.FromCornerXToMiddleXAxis((float)(GameContainer.mainGrid.ActualWidth - 30)) ||
                 translateTransform.X < GlobalMethods.FromCornerXToMiddleXAxis(30)) 
             { 
-                this.velocity.x *= -1; 
+                this.velocity.x *= -1;
             }
 
             if (translateTransform.Y > GlobalMethods.FromCornerYToMiddleYAxis((float)(GameContainer.mainGrid.ActualHeight - 30)) ||
@@ -37,6 +37,40 @@ namespace Pongaline.Classes
 
             translateTransform.X += this.velocity.x;
             translateTransform.Y += this.velocity.y;
+
+            if (translateTransform.X > GlobalMethods.FromMiddleXToCornerXAxis((float)(GameContainer.mainGrid.ActualWidth - GlobalVariables.fieldMargin)))
+            {
+                var border = GameContainer.mainGrid.Children.FirstOrDefault(ui => ui is Border) as Border;
+                var grid = border.Child as Grid;
+                var textblock = grid.Children.FirstOrDefault(ui => ui is TextBlock && ((TextBlock)ui).Name == "TextBlock_LeftScore") as TextBlock;
+                textblock.Text = (int.Parse(textblock.Text) + 1).ToString();
+                Reset();
+            }
+
+            if (translateTransform.X < GlobalMethods.FromMiddleXToCornerXAxis(GlobalVariables.fieldMargin))
+            {
+                var border = GameContainer.mainGrid.Children.FirstOrDefault(ui => ui is Border) as Border;
+                var grid = border.Child as Grid;
+                var textblock = grid.Children.FirstOrDefault(ui => ui is TextBlock && ((TextBlock)ui).Name == "TextBlock_RightScore") as TextBlock;
+                textblock.Text = (int.Parse(textblock.Text) + 1).ToString();
+                Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            TranslateTransform translateTransform = this.image.RenderTransform as TranslateTransform;
+
+            translateTransform.X = 0;
+            translateTransform.Y = 0;
+
+            this.position.x = 0;
+            this.position.y = 0;
+
+            Random random = new Random();
+
+            velocity.x = random.Next(-20, 20);
+            velocity.y = random.Next(-20, 20);
         }
     }
 }
